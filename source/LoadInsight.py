8e1f08f92184d9a9bb9305468771434bfeb53fbc
David P. Chassin <dchassin@slac.stanford.edu>
2019-06-05 16:16:20 -0700

    Working update

--- source/LoadInsight.py
+++ source/LoadInsight.py
@@ -1,25 +1,45 @@
 import pandas as pd
+import numpy as np
+import os
 
 def main() :
 	"""Dummy main to test basic pipeline functionality"""
+
+	# test 1: nop(NULL,NULL)
 	input1 = DataObject()
 	output1 = DataObject()
 	transform = Transformation(inputs=[input1],outputs=[output1],function=nop)
 	p1 = Pipeline()
 	p1.add(transform)
 	p1.run()
+	print(output1)
+
+	# test 2: twice(random,random2)
+	input2 = DataObject(name="random",data=pd.DataFrame(np.random.randn(5,7)))
+	output2 = DataObject()
+	double_it = Transformation(inputs=[input2],outputs=[output2],function=twice)
+	p2 = Pipeline()
+	p2.add(double_it)
+	p2.run()
+	print(input2,'->',output2)
+
+
+def null(x):
+	return [pd.DataFrame()]
 
 def nop(x):
-	return(x)
+	return([x[0]])
 
-def rand(x,N,M):
-	import numpy as np
-	pd.DataFrame(np.random.randn(N,M))
+def twice(x):
+	return([x[0]*2])
+	
 
 class DataObject:
 	"""DataObject hold all data artifacts as pandas DataFrames 
 	   and associated storage specifications.
 	"""
+
+	# default global values -- override using a config file
 	local_storage = {"path":""}
 	remote_storage = {} # TODO: from infrastucture
 
@@ -47,34 +67,32 @@ class DataObject:
 			else:
 				self.data = pd.DataFrame(localname)
 
-		elif data is pd.DataFrame :
+		elif type(data) is pd.DataFrame :
 			self.data = data
 
-		elif data is None:
+		elif data == None:
 			self.data = pd.DataFrame()
-
-		if convert is dict:
-			for column, action in convert.items() :
-				if column in self.data.headers :
-					self.data[column] = map(action,self.data[column])
+		else:
+			raise Exception("data is not valid")
 
 	def __str__(self):
-		return("%s: %s" % (self.name,self.data))
+		return("{'name':'%s'; 'data':%s}" % (self.name,self.data))
 
 	def get_localfile(self):
-		return local_storage + self.name
+		return self.local_storage["path"] + self.name + ".csv"
 
 	def get_remotefile(self): 
 		return None # TODO: from infrastucture
 
 	def write(self) :
-		print("Writing %s..." % (self.name))
-		self.data.to_csv()
+		#print("Writing %s..." % (self.name))
+		self.data.to_csv(self.get_localfile())
 
 	def read(self) :
-		print("Reading %s..." % (self.name))
-		if self.name != "NULL" :
-			self.data = pd.DataFrame(get_localfile(),convert=convert)
+		filename = self.get_localfile()
+		#print("Reading %s into %s..." % (filename,self.name))
+		if self.name != "NULL" and os.path.exists(filename):
+			self.data = pd.DataFrame.from_csv(filename)
 		else :
 			self.data = pd.DataFrame()
 
@@ -100,10 +118,16 @@ class Transformation :
 				item.read()
 
 		if autorun :
-			outputs = map(self.function,inputs)
-
+			args = []
+			for n in range(len(self.inputs)):
+				args.append(self.inputs[n].data)
+			results = self.function(args)
+			if len(results) != len(self.outputs) :
+				raise Exception("function %s does not returned expected number of output (expected %d, but got %d)" % (self.function,len(self.outputs),len(results)))
+			for n in range(len(outputs)):
+				self.outputs[n].data = results[n]
 			if autowrite :
-				for output in outputs:
+				for output in self.outputs:
 					output.write()
 
 class Pipeline :
