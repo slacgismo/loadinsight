ad9df79098a9c9fef4ed46190a5a8e38a67fd27c
Jonathan Goncalves <jonathan.goncalves@gmail.com>
2019-06-11 16:16:02 -0700

    Add in the initial setup for data reading

--- generics/artifact.py
+++ generics/artifact.py
@@ -8,7 +8,13 @@ class ArtifactDataManager(object):
     def __init__(self):
         print("I'm a new artifact data manager")
 
-
+    def load_data(self, data_files):
+        data_map = {}
+        for filename in data_files:
+            logger.info(filename)
+            data_map[filename] = 1234
+        return data_map
+        
 class ArtifactDataWrapper(object):
     def __init__(self):
         print("I'm a new artifact data wrapper")
--- pipelines/rbsa/tasks/normalize.py
+++ pipelines/rbsa/tasks/normalize.py
@@ -9,9 +9,15 @@ class Normalizer(t.Task):
     def __init__(self, name):
         super().__init__(self)
         self.name = name
+        self.my_data_files = ['test.csv']
         self.task_function = self._task
+        self.df = None
+
+    def _get_data(self):
+        self.df = self.load_data(self.my_data_files)
 
     def _task(self):
-        logger.info(self.data)
+        self._get_data()
+        logger.info(self.df)
         return 2 * 10 * 50
     
