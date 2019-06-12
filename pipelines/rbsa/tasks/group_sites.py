ae89c09aa9567694c06968bf4e3b373a2d3140fe
gismo <berk@serbet.com>
2019-06-12 11:36:03 -0700

    syntax fix

--- pipelines/rbsa/tasks/group_sites.py
+++ pipelines/rbsa/tasks/group_sites.py
@@ -1,6 +1,6 @@
 import logging
 from generics import task as t
-
+import pandas as pd
 
 logger = logging.getLogger('LCTK_APPLICATION_LOGGER')
 
@@ -20,14 +20,16 @@ class SitesGrouper(t.Task):
         # self.map = self.load_data(self.my_data_files[1])
 
         #override
-        self.df = pd.read_csv('../../local_data/rbsa_cleandata.csv')
-        self.map = pd.read_csv('../../local_data/rbsa_zipmap.csv')
+        self.df = pd.read_csv('local_data/rbsa_cleandata.csv')
+        self.map = pd.read_csv('local_data/rbsa_zipmap.csv')
 
     def _task(self):
         self._get_data()
         logger.info(self.df)
 
-        all_sites = df.index.get_level_values('siteid').unique()
+        self.df.siteid = self.df.siteid.astype(str)
+
+        all_sites = self.df['siteid'].unique()
 
         # guarantee dataframe correct types
         self.map.siteid = self.map.siteid.astype(str)
@@ -35,6 +37,30 @@ class SitesGrouper(t.Task):
 
         # keys are sites, values are zipcodes
         site_zipmap = dict(zip(self.map['siteid'], self.map['postcode']))
+
+        # new dict that maps 3 digit zipcodes to sites in zipcode
+        zip_sitemap = {}
+
+        for site in all_sites:
+            zipcode = site_zipmap[site]
+            zipcode3digit = zipcode[:3]
+            
+            if zipcode3digit in zip_sitemap.keys(): 
+                zip_sitemap[zipcode3digit].append(site)
+            else:
+                zip_sitemap[zipcode3digit] = [site]
+
+        # maps 3 digit zipcodes to list of 5 digit zipcodes, for correlation
+        zipcode5map = {}
+
+        for site in all_sites:
+            zipcode = site_zipmap[site]
+            zipcode3digit = zipcode[:3]
+            
+            if zipcode3digit in zipcode5map.keys(): 
+                zipcode5map[zipcode3digit].append(zipcode)
+            else:
+                zipcode5map[zipcode3digit] = [zipcode]
 
         return 2 * 10 * 50
     
