7b015d214062f860639ca229f083caa81a4d4e2e
gismo <berk@serbet.com>
2019-06-13 09:13:05 -0700

    rbsa headcool indexer initialize

--- pipelines/rbsa/rbsa.py
+++ pipelines/rbsa/rbsa.py
@@ -1,6 +1,7 @@
 import logging
 from pipelines.rbsa.tasks import normalize
 from pipelines.rbsa.tasks import group_sites
+from pipelines.rbsa.tasks import index_heatcool
 from generics import pipeline as p, task as t
 
 
@@ -14,9 +15,13 @@ class RbsaPipeline(p.Pipeline):
     def create_tasks(self):
         # multiply_task = normalize.Normalizer('multiply_task')
         # self.pipeline.add_task(multiply_task)
+
         site_grouping_task = group_sites.SitesGrouper('site_grouping_task')
         self.pipeline.add_task(site_grouping_task)
 
+        heatcool_indexing_task = index_heatcool.HeatcoolIndexer('heatcool_indexing_task')
+        self.pipeline.add_task(heatcool_indexing_task)
+        
     def execute(self):
         """
         Run all the tasks in this pipeline
---
+++ pipelines/rbsa/tasks/index_heatcool.py
@@ -0,0 +1,51 @@
+import logging
+from generics import task as t
+import pandas as pd
+
+logger = logging.getLogger('LCTK_APPLICATION_LOGGER')
+
+
+class HeatcoolIndexer(t.Task):
+    def __init__(self, name):
+        super().__init__(self)
+        self.name = name
+        self.my_data_files = ['local_data/rbsa_weather/594.csv',
+                            'local_data/rbsa_weather/596.csv',
+                            'local_data/rbsa_weather/597.csv',
+                            'local_data/rbsa_weather/833.csv',
+                            'local_data/rbsa_weather/835.csv',
+                            'local_data/rbsa_weather/836.csv',
+                            'local_data/rbsa_weather/837.csv',
+                            'local_data/rbsa_weather/838.csv',
+                            'local_data/rbsa_weather/970.csv',
+                            'local_data/rbsa_weather/971.csv',
+                            'local_data/rbsa_weather/972.csv',
+                            'local_data/rbsa_weather/973.csv',
+                            'local_data/rbsa_weather/974.csv',
+                            'local_data/rbsa_weather/980.csv',
+                            'local_data/rbsa_weather/981.csv',
+                            'local_data/rbsa_weather/982.csv',
+                            'local_data/rbsa_weather/983.csv',
+                            'local_data/rbsa_weather/984.csv',
+                            'local_data/rbsa_weather/985.csv',
+                            'local_data/rbsa_weather/988.csv',
+                            'local_data/rbsa_weather/989.csv',
+                            'local_data/rbsa_weather/990.csv',
+                            'local_data/rbsa_weather/991.csv',
+                            'local_data/rbsa_weather/992.csv',
+                            'local_data/rbsa_weather/993.csv'
+                            ]
+        self.task_function = self._task
+        self.df = None
+
+    def _save_data(self):
+        self.save_data(self.df)
+
+    def _get_data(self):
+        self.df = self.load_data(self.my_data_files)
+
+    def _task(self):
+        self._get_data()
+        logger.info(self.df)
+        return 2 * 10 * 50
+    
