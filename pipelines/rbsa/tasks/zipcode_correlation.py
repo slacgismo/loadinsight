39aa968507ad6dec7ea24d0d0307b905b96bca91
gismo <berk@serbet.com>
2019-06-17 11:31:08 -0700

    correlation setup

--- pipelines/rbsa/tasks/zipcode_correlation.py
+++ pipelines/rbsa/tasks/zipcode_correlation.py
@@ -11,58 +11,46 @@ class ZipcodeCorrelation(t.Task):
     def __init__(self, name):
         super().__init__(self)
         self.name = name
-        self.data_files = [
-            # { 'name': 'area_loads.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/594.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/596.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/597.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/598.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/833.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/835.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/836.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/837.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/838.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/970.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/971.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/972.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/973.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/974.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/980.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/981.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/982.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/983.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/984.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/985.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/988.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/989.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/990.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/991.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/992.csv', 'read_type': SupportedFileReadType.DATA },
-            # { 'name': 'noaa/993.csv', 'read_type': SupportedFileReadType.DATA }
+
+        self.input_artifact_full_zipcodes = 'full_zipcodes.csv'
+        self.input_artifact_projection_locations = 'PROJECTION_LOCATIONS.json'
+
+        # these will be used to generate list of input files
+        self.pre_data_files = [ 
+            { 'name': self.input_artifact_full_zipcodes, 'read_type': SupportedFileReadType.DATA },
+            { 'name': self.input_artifact_projection_locations, 'read_type': SupportedFileReadType.CONFIG }
         ]
+
+        self.pre_data_map = self.load_data(self.pre_data_files) 
+        self.full_zipcodes = list(self.pre_data_map['full_zipcodes.csv']['zipcodes'])  
+        self.projection_locations = list(self.pre_data_map['PROJECTION_LOCATIONS.json']['cities'].keys())
+
+        self.data_files = []
+
+        for location in self.full_zipcodes:
+            self.data_files.append({ 'name': 'tmy_base/'+str(location)+'.csv', 'read_type': SupportedFileReadType.DATA })
+
+        for location in self.projection_locations:
+            self.data_files.append({ 'name': 'tmy_target/'+str(location)+'.csv', 'read_type': SupportedFileReadType.DATA })
+
         self.task_function = self._task
-        self.output_artifact_enduse_loads = 'enduse_loads.csv'
+        self.output_artifact_correlation_matrix = 'correlation_matrix.csv'
         
         self.data_map = None
         self.df = None
-
-        # these should be read from config, they are different for RBSA and CEUS
-        self.theat = 15
-        self.tcool = 25
 
     def _save_data(self):
         self.save_data(self.df)
 
     def _get_data(self):
-        self.data_map = self.load_data(self.data_files) 
-        self.df = self.data_map['area_loads.csv']        
+        self.data_map = self.load_data(self.data_files)       
 
     def _task(self):
         self._get_data()
         logger.info(self.df)
 
-        self.validate(enduse_loads)
-        self.save_data({self.output_artifact_enduse_loads: enduse_loads})
+        self.validate()
+        self.save_data({self.output_artifact_correlation_matrix: correlation_matrix})
 
     def validate(self, df):
         """
