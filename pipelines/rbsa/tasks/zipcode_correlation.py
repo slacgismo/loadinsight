b3a3be32cffb2ab6d8d47cd8a49af4e7bf605821
gismo <berk@serbet.com>
2019-06-17 17:37:23 -0700

    correlation complete

--- pipelines/rbsa/tasks/zipcode_correlation.py
+++ pipelines/rbsa/tasks/zipcode_correlation.py
@@ -1,5 +1,6 @@
 import logging
 import pandas as pd
+import numpy as np
 from generics import task as t
 from generics.file_type_enum import SupportedFileReadType
 
@@ -21,6 +22,16 @@ class ZipcodeCorrelation(t.Task):
             { 'name': self.input_artifact_projection_locations, 'read_type': SupportedFileReadType.CONFIG }
         ]
 
+        self.task_function = self._task
+        self.output_artifact_correlation_matrix = 'correlation_matrix.csv'
+        
+        self.data_map = None
+        self.df = None
+
+    def _save_data(self):
+        self.save_data(self.df)
+
+    def _get_data(self):
         self.pre_data_map = self.load_data(self.pre_data_files) 
         self.full_zipcodes = list(self.pre_data_map['full_zipcodes.csv']['zipcodes'])  
         self.projection_locations = list(self.pre_data_map['PROJECTION_LOCATIONS.json']['cities'].keys())
@@ -33,24 +44,66 @@ class ZipcodeCorrelation(t.Task):
         for location in self.projection_locations:
             self.data_files.append({ 'name': 'tmy_target/'+str(location)+'.csv', 'read_type': SupportedFileReadType.DATA })
 
-        self.task_function = self._task
-        self.output_artifact_correlation_matrix = 'correlation_matrix.csv'
-        
-        self.data_map = None
-        self.df = None
-
-    def _save_data(self):
-        self.save_data(self.df)
-
-    def _get_data(self):
         self.data_map = self.load_data(self.data_files)       
 
     def _task(self):
         self._get_data()
         logger.info(self.df)
 
-        self.validate()
-        self.save_data({self.output_artifact_correlation_matrix: correlation_matrix})
+        correlation_metrics = ['Temperature', 'Solar Zenith Angle', 'GHI', 'DHI', 'DNI', 'Wind Speed', 'Wind Direction', 'Relative Humidity']    
+        correlation_matrix = pd.DataFrame(0, index=self.projection_locations, columns=self.full_zipcodes)
+
+        for base in self.full_zipcodes:
+            for target in self.projection_locations:
+
+                base_filename = 'tmy_base/'+str(base)+'.csv'
+                target_filename = 'tmy_target/'+str(target)+'.csv'
+
+                base_weather = self.data_map[base_filename]
+                target_weather = self.data_map[target_filename]
+
+                base_weather = base_weather.set_index(base_weather.columns[0])
+                target_weather = target_weather.set_index(target_weather.columns[0])
+
+                if base_weather.shape != target_weather.shape:
+                    logger.exception(f'Task {self.name} did not pass validation. TMY weather data sizes do not match for {base} and {target}.')
+                    continue
+
+                correlation_vals = []
+                for metric in correlation_metrics:       
+                    correlation_vals.append(np.ma.corrcoef(base_weather[metric], target_weather[metric])[0][1])
+
+                correlation = sum(correlation_vals)/len(correlation_vals)
+                correlation_matrix.ix[target,base] = correlation
+
+        correlation_matrix = self.convert_coef_3digit(correlation_matrix)
+
+        self.validate(correlation_matrix)
+        self.on_complete({self.output_artifact_correlation_matrix: correlation_matrix})
+
+    def convert_coef_3digit(self, df):
+        """ Convert correlation of coefficient array from 5 digit zipcodes to 3
+        """
+        zipcodes_3digit = set()
+
+        for zipcode in self.full_zipcodes:
+            zipcodes_3digit.add(str(zipcode)[0:3])
+
+        new_coef = pd.DataFrame(columns=zipcodes_3digit, index=self.projection_locations)
+
+        for city in self.projection_locations:
+            row = df.loc[df.index == city].squeeze()
+            ordered_row = row.sort_values(ascending=False)
+
+            for index in ordered_row.index:
+                cell_val = new_coef.loc[city][str(index)[:3]]
+
+                if cell_val != cell_val:
+                    new_coef.at[city,str(index)[:3]] = ordered_row[index]
+
+        new_coef = new_coef.reindex(sorted(new_coef.columns), axis=1)
+
+        return new_coef
 
     def validate(self, df):
         """
@@ -58,7 +111,7 @@ class ZipcodeCorrelation(t.Task):
         """
         logger.info(f'Validating task {self.name}')
         if df.isnull().values.any():
-            logger.exception(f'Task {self.name} did not pass validation. Error found during grouping of sites to zip codes.')
+            logger.exception(f'Task {self.name} did not pass validation. Error found during generating correlation matrix.')
             self.did_task_pass_validation = False
             self.on_failure()
 
