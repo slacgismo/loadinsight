59f400051b5723e252ff809747d44873a105724b
gismo <berk@serbet.com>
2019-06-18 17:31:06 -0700

    removing gas fraction

--- pipelines/rbsa/rbsa.py
+++ pipelines/rbsa/rbsa.py
@@ -6,7 +6,8 @@ from pipelines.rbsa.tasks import (
     undiscount_gas, 
     index_heatcool, 
     normalize_totals,
-    zipcode_correlation
+    zipcode_correlation, 
+    discount_gas
 )
 
 logger = logging.getLogger('LCTK_APPLICATION_LOGGER')
@@ -24,20 +25,23 @@ class RbsaPipeline():
             self.create_tasks()
 
     def create_tasks(self):
-        site_grouping_task = group_sites.SitesGrouper('site_grouping_task')
-        self.pipeline.add_task(site_grouping_task)
+        # site_grouping_task = group_sites.SitesGrouper('site_grouping_task')
+        # self.pipeline.add_task(site_grouping_task)
 
-        heatcool_indexing_task = index_heatcool.HeatcoolIndexer('heatcool_indexing_task')
-        self.pipeline.add_task(heatcool_indexing_task)
+        # heatcool_indexing_task = index_heatcool.HeatcoolIndexer('heatcool_indexing_task')
+        # self.pipeline.add_task(heatcool_indexing_task)
 
-        undiscount_gas_task = undiscount_gas.UndiscountGas('undiscount_gas_task')
-        self.pipeline.add_task(undiscount_gas_task)
+        # undiscount_gas_task = undiscount_gas.UndiscountGas('undiscount_gas_task')
+        # self.pipeline.add_task(undiscount_gas_task)
 
-        normalize_totals_task = normalize_totals.NormalizeTotals('normalize_totals_task')
-        self.pipeline.add_task(normalize_totals_task)
+        # normalize_totals_task = normalize_totals.NormalizeTotals('normalize_totals_task')
+        # self.pipeline.add_task(normalize_totals_task)
 
-        correlation_task = zipcode_correlation.ZipcodeCorrelation('correlation_task')
-        self.pipeline.add_task(correlation_task)
+        # correlation_task = zipcode_correlation.ZipcodeCorrelation('correlation_task')
+        # self.pipeline.add_task(correlation_task)
+
+        discount_gas_task = discount_gas.DiscountGas('discount_gas_task')
+        self.pipeline.add_task(discount_gas_task)
 
     def execute(self):
         """
