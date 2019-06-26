1982b91da60ac36e789f466bbc73e8dece811938
gismo <berk@serbet.com>
2019-06-25 23:44:51 -0700

    ceus full pipeline

--- pipelines/ceus/ceus.py
+++ pipelines/ceus/ceus.py
@@ -11,7 +11,7 @@ from pipelines.ceus.tasks import (
     fcz_correlation,
     project_loadshapes,
     discount_gas,
-    # normalize_loadshapes
+    normalize_loadshapes
 )
 
 
@@ -31,26 +31,26 @@ class CeusPipeline():
             self.create_tasks()
 
     def create_tasks(self):
-        # undiscount_gas_task = undiscount_gas.UndiscountGas('undiscount_gas_task')
-        # self.pipeline.add_task(undiscount_gas_task)
+        undiscount_gas_task = undiscount_gas.UndiscountGas('undiscount_gas_task')
+        self.pipeline.add_task(undiscount_gas_task)
 
-        # normalize_totals_task = normalize_totals.NormalizeTotals('normalize_totals_task')
-        # self.pipeline.add_task(normalize_totals_task)
+        normalize_totals_task = normalize_totals.NormalizeTotals('normalize_totals_task')
+        self.pipeline.add_task(normalize_totals_task)
 
-        # correlation_task = fcz_correlation.FczCorrelation('correlation_task')
-        # self.pipeline.add_task(correlation_task)
+        correlation_task = fcz_correlation.FczCorrelation('correlation_task')
+        self.pipeline.add_task(correlation_task)
 
-        # find_sensitivities_task = find_sensitivities.FindSensitivities('find_sensitivities_task')
-        # self.pipeline.add_task(find_sensitivities_task)
+        find_sensitivities_task = find_sensitivities.FindSensitivities('find_sensitivities_task')
+        self.pipeline.add_task(find_sensitivities_task)
 
-        # project_loadshapes_task = project_loadshapes.ProjectLoadshapes('project_loadshapes_task')
-        # self.pipeline.add_task(project_loadshapes_task)
+        project_loadshapes_task = project_loadshapes.ProjectLoadshapes('project_loadshapes_task')
+        self.pipeline.add_task(project_loadshapes_task)
 
         discount_gas_task = discount_gas.DiscountGas('discount_gas_task')
         self.pipeline.add_task(discount_gas_task)
 
-        # normalize_loadshapes_task = normalize_loadshapes.NormalizeLoadshapes('normalize_loadshapes_task')
-        # self.pipeline.add_task(normalize_loadshapes_task)
+        normalize_loadshapes_task = normalize_loadshapes.NormalizeLoadshapes('normalize_loadshapes_task')
+        self.pipeline.add_task(normalize_loadshapes_task)
 
     def _create_results_storage(self, storage_name=None):
         try:
