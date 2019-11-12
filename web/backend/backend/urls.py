37a5e74aba40ee72e68583b121ee059ab077a0bf
SZhengyu <yykl1996@126.com>
2019-11-11 16:38:24 -0800

    removed unused apis and get methods of retrieving pictures

--- web/backend/backend/urls.py
+++ web/backend/backend/urls.py
@@ -8,19 +8,6 @@ urlpatterns = [
     path('api/run_pipeline/', run_pipeline),
     path('api/run_pipeline/<str:pipeline_name>/', run_pipeline),
     path('api/my_executions/', get_executions),
-    # path('api/my_executions/<str:execution_id>/', get_executions_by_id),
-    # path('api/my_executions/<str:execution_id>/results/<str:result_dir>/', get_executions_by_result_dir),
-    # path('api/my_executions/<str:execution_id>/results/<str:result_dir>/images/<str:image_name>/',
-    #      get_executions_by_image),
-    # path('api/my_executions/<str:execution_id>/results/<str:result_dir>/city/<str:city_name>/',
-    #      get_executions_by_city),
-    # path('api/my_executions/<str:execution_id>/results/<str:result_dir>/state/<str:state_name>/',
-    #      get_executions_by_state),
-    # path('api/my_executions/<str:execution_id>/results/<str:result_dir>/content/<str:content_name>/',
-    #      get_executions_by_content),
-    # path('api/my_executions/<str:execution_id>/results/<str:result_dir>/city/<str:city_name>/content/<str:content_name>/',
-    #      get_executions_by_city_and_content),
-
     re_path('^api/my_executions/(?P<execution_id>\w+)/(?P<result_dir>\w+)/(?P<city_name>\w+)/(?P<state_name>\w+)/'
         '(?P<content_name>\w+)', get_executions_result),
     # keep the path below at bottom because it is used to catch all other urls served by react
