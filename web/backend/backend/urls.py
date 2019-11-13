b32faef34207f4b75308f6b822af56a466246569
wjt <jtwang.me@gmail.com>
2019-11-12 20:02:46 -0800

    refactor front-end

--- web/backend/backend/urls.py
+++ web/backend/backend/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('api/executions/configs/', get_pipeline_configs),  # GET pipeline configs
-    path('api/executions/', handle_executions),  # POST to execute, GET to get all executions
-    re_path(r'api/executions/(?P<execution_id>\w)/', handle_executions),
-    re_path(r'api/executions/(?P<execution_id>\w)/results/(?P<result_dir>\w+)/', handle_executions), # GET, but the GET body contains city name, state name, and content name
+    path('executions/configs/', get_pipeline_configs),  # GET pipeline configs
+    path('executions/', handle_executions),  # POST to execute, GET to get all executions
+    re_path(r'executions/(?P<execution_id>\w)/', handle_executions),
+    re_path(r'executions/(?P<execution_id>\w)/results/(?P<result_dir>\w+)/', handle_executions), # GET, but the GET body contains city name, state name, and content name
 ]
 
 
