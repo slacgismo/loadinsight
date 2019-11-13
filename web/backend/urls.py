4b475b8478655c779749eb6689fad361fac02fec
wjt <jtwang.me@gmail.com>
2019-11-12 17:30:14 -0800

    change url patterns

--- web/backend/urls.py
+++ web/backend/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.conf.urls import include, url
-from django.urls import path
+from django.urls import re_path
+from django.views.generic import TemplateView
 
 urlpatterns = (
-    path('admin/', admin.site.urls),
-    url(r"^auth/", include("djoser.urls.base")),
-    url(r"^auth/", include("djoser.urls.authtoken")),
-    # keep the path below at bottom because it is used to catch all urls other than auth
-    path('', include('backend.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^auth/', include('djoser.urls.base')),
+    url(r'^auth/', include('djoser.urls.authtoken')),
+    url(r'^api/', include('backend.urls')),
+    # keep the path below at bottom because it is used to catch all urls other than admin, auth and api
+    re_path(r'^(?!(admin/|auth/|api/)).*$', TemplateView.as_view(template_name='index.html'), name='index')
 )
