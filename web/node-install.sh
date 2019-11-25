ad215769949d0d5a0994e1a815463a2c016a1cba
Junjie Que <marioquer@hotmail.com>
2019-11-24 18:42:27 -0800

    update nodejs installation script

--- web/node-install.sh
+++ web/node-install.sh
@@ -1,5 +1,14 @@
 NODE_VERSION=13.0.1
-ARCH=x64
+ARCH= && dpkgArch="$(dpkg --print-architecture)" \ 
+ && case "${dpkgArch##*-}" in \ 
+ amd64) ARCH='x64';; \ 
+ ppc64el) ARCH='ppc64le';; \ 
+ s390x) ARCH='s390x';; \ 
+ arm64) ARCH='arm64';; \ 
+ armhf) ARCH='armv7l';; \ 
+ i386) ARCH='x86';; \ 
+ *) echo "unsupported architecture"; exit 1 ;; \ 
+ esac
 PLATFORM=$(uname -s | tr '[:upper:]' '[:lower:]')
 
 buildDeps='xz-utils' 
