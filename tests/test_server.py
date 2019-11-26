00984eeb38f23b1b237097f6272092159ee3dd2a
Junjie Que <marioquer@hotmail.com>
2019-11-26 15:58:35 -0800

    test web page display in circleci build

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,7 +1,13 @@
 import unittest
+import requests
 from settings import base
 
 class TestServerSetup(unittest.TestCase):
 
     def test_server_build(self):
-        self.assertEqual(base.DEBUG, False)
+        # test front end page
+        response = requests.get('http://localhost:8000')
+        self.assertEqual(response.status_code, 200)
+
+        html = response.text
+        self.assertEqual('<title>LoadInsight</title>' in html, True)
