19ac6da68d3e2afd712ea98664cb49a5eb2b5851
Jonathan Goncalves <jonathan.goncalves@gmail.com>
2020-03-26 07:58:17 -0700

    remove email validation

--- web/backend/settings.py
+++ web/backend/settings.py
@@ -74,7 +74,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 DJOSER = {
-    "SEND_ACTIVATION_EMAIL": True,
+    "SEND_ACTIVATION_EMAIL": False,
     "PASSWORD_RESET_CONFIRM_URL": "password/reset/confirm/{uid}/{token}",
     "USERNAME_RESET_CONFIRM_URL": "username/reset/confirm/{uid}/{token}",
     "ACTIVATION_URL": "activate/{uid}/{token}",
